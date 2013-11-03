db82a1304b9d50cd0287a4aa62130aba48ed77a7
Bugeda.Alexey <Bugeda@mail.ru>
2013-11-03 18:59:55 +0200

    Add Supplier class

--- src/Supplier.java
+++ src/Supplier.java
@@ -9,20 +9,27 @@ public class Supplier {
 		//get supplier to work
 		name=sName;
 		//assortiment = set assortiment;
+		Shop.manager.setAssortiment(assortiment);
 	}
 	
 	public String createAccount() {
-		//manager.sentproduct
+		//it's may be other class "account"
 		return account="new account";	
 	}
 	
-	public static void setPayAccount(Object payAccount) {
-		//sendproduct
+	public static void setPayAccount(Order order) {
+		//supplier may check bank account and than
+		sendProduct(order);
 	}
 
-	public void  sendproduct() {
-		// TODO Auto-generated method stub
-		//sendproduct
-	}
+	public static void sendProduct(Order order) {
+		//say manager that product sent at shop
+		if (order.getStatus().equals("advancepay"))
+			{
+			//send product to shop
+			order.setStatus("send");
+			Shop.manager.sentProduct(order);
+			}
+		}
 
 }
